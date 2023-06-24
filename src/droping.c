b2bb7f086cbf52e3f86f8becd4a5c28c95451e8a
arstgr <78465331+arstgr@users.noreply.github.com>
2023-06-24 15:08:15 -0400

    removed extra text at the beginning of source codes

--- src/droping.c
+++ src/droping.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) $2009$ by $Amirreza Rastegari$                          *
+ *   $Amirreza Rastegari$                                                  *
  *   $arstgri@gmail.com$                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
